21cd670e16cd1ffe253ec9285a5f96198efe62a5
Matthew Stirnweiss <mpstirn@outlook.com>
2018-03-28 12:39:44 -0400

    mvp working

--- game/index.js
+++ game/index.js
@@ -1,6 +1,33 @@
 import {Map} from 'immutable'
 
-export default function reducer(state, action) {
+const initialState = { board: Map(), turn: 'X' }
+export const move = (player, [row, col]) => {
+  console.log({
+    type: 'MOVE',
+    player,
+    position: [row, col]
+  })
+  return {
+    type: 'MOVE',
+    player,
+    position: [row, col]
+  }
+}
+
+export default function reducer(state = initialState, action) {
   // TODO
-  return state
+  console.log('Reducer: ', action)
+  
+  if (action.player === 'X') {
+    return {
+      board: state.board.setIn(action.position, action.player),
+      turn: 'O'
+    } 
+  } else if (action.player === 'O') {
+    return {
+      board: state.board.setIn(action.position, action.player),
+      turn: 'X'
+    }
+  }
+  return state;
 }
